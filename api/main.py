b34c2f1bb3a6150460d94ea61afc26ca855fc7b7
naka520 <yashipipi0520@gmail.com>
2023-11-04 16:14:53 +0900

    [modify]main.pyにcorsにひっかからないように修正

--- api/main.py
+++ api/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 全てのオリジンを許可
+    allow_credentials=True,
+    allow_methods=["*"],  # 全てのメソッドを許可
+    allow_headers=["*"],  # 全てのヘッダーを許可
+)
 
 
 @app.get("/hello")
